dc027ca1415323973b5c3399318f6122b2d036e1
Keson_liu <604957104@qq.com>
2016-11-07 14:34:30 +0800

    another test domain

---
+++ src/main/java/com/liukecheng/domain/User.java
@@ -0,0 +1,47 @@
+package com.liukecheng.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+	private int id;
+	
+	private int age;
+	
+	private String name;
+	
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
