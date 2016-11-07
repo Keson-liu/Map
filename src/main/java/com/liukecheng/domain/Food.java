6e0f06fcea80380efaade2e5c5dea40aca2d849c
Keson_liu <604957104@qq.com>
2016-11-07 14:45:05 +0800

    this is a food class

---
+++ src/main/java/com/liukecheng/domain/Food.java
@@ -0,0 +1,45 @@
+package com.liukecheng.domain;
+
+public class Food {
+
+	private int id;
+	
+	private double price;
+	
+	private String name;
+	
+	private int star;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStar() {
+		return star;
+	}
+
+	public void setStar(int star) {
+		this.star = star;
+	}
+	
+}
