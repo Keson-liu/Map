f9bd4326590cf7b40bf5924760fb4b50122794fc
Keson_liu <604957104@qq.com>
2016-11-07 11:36:35 +0800

    sdf

---
+++ src/main/java/com/liukecheng/domain/Test1.java
@@ -0,0 +1,15 @@
+package com.liukecheng.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		List<String> testList = new ArrayList<String>();
+		testList.add("1");
+		testList.add("2");
+		
+	}
+}
